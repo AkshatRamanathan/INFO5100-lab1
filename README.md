37fddf65558718c9843bcf33b11a4feb24d25708
AkshatRamanathan <ramanathan.ak@northeastern.edu>
2023-01-19 14:20:29 -0500

    lab one init commit
    lab one init commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # INFO5100-lab1
- INFO5100-lab1
+
+INFO5100-lab1
+
+# Description
+
+Simple form application for user input and displaying data
---
+++ src/data/User.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package data;
+
+/**
+ *
+ * @author akshb
+ */
+public class User {
+
+    String firstName;
+    String lastName;
+    String city;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+    
+    
+
+}
